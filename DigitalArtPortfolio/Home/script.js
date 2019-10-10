1400d4caa5cec9412599fbe55d98300019f8befd
A-Zahra <z.a.ahmadi18@gmail.com>
2019-10-09 21:19:23 -0400

    Edit commens

--- DigitalArtPortfolio/Home/script.js
+++ DigitalArtPortfolio/Home/script.js
@@ -1,6 +1,9 @@
 var slideIndex;
 
+// By storing the pages location in local storage makes it possible
+// to go back and forth between the home page slides and other pages of the website
 window.onload = function(){
+// See what is the actual size of the screen
 console.log(window.screen.width);
 if(window.screen.width>1399){
   if(localStorage.getItem('previous') ==='logosPage' ||
@@ -9,22 +12,18 @@ localStorage.getItem('previous') ==='animationsPage'){
     if(localStorage.getItem('next') ==='aboutPage'){
       console.log("about");
       slideIndex = 2;
-
     }
     else if(localStorage.getItem('next') ==='portfolioPage'){
       console.log("about");
       slideIndex = 3;
-
     }
     else if(localStorage.getItem('next') ==='contactPage'){
       console.log("about");
       slideIndex = 4;
-
     }
     else{
         slideIndex = 1;
     }
-  //  console.log("logos");
   }
   else{
     console.log('previous is not logos');
@@ -36,14 +35,8 @@ localStorage.setItem('next','');
 }
 else{
    console.log("smaller");
-  // slideIndex=1;
-  // showSlides(slideIndex);
-
 }
 }
-
-
-
 
 // Next/previous controls
 function plusSlides(n) {
@@ -55,6 +48,9 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+// showSlides()
+//
+// To make an slide show
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -77,7 +73,4 @@ function showSlides(n) {
   // Contact me information section slide down
   $(".C-column").hide();
   $(".C-column").slideDown(2000);
-  /*$("#Zahra-text").hide();
-  $("#Zahra-pic").fadeOut(3000);*/
-
 }
